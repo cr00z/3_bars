84e5055d0e563cdb981e86d25ae5169251021149
cr00z <netrebinr@gmail.com>
2019-03-30 21:11:24 +0700

    make a second attempt

--- bars.py
+++ bars.py
@@ -1,5 +1,4 @@
 import json
-import sys
 import math
 
 
@@ -9,46 +8,23 @@ def load_data(filepath):
 
 
 def get_biggest_bar(data):
-    max_seats_count = 0
-    biggest_bars = []
-    for bar in data['features']:
-        seats_count = bar['properties']['Attributes']['SeatsCount']
-        if max_seats_count > seats_count:
-            continue
-        if max_seats_count < seats_count:
-            max_seats_count = seats_count
-            biggest_bars = []
-        biggest_bars.append(bar)
-    return biggest_bars
+    seats_count = lambda x: x['properties']['Attributes']['SeatsCount']
+    return max(data['features'], key=seats_count)
 
 
 def get_smallest_bar(data):
-    min_seats_count = sys.maxint
-    smallest_bars = []
-    for bar in data['features']:
-        seats_count = bar['properties']['Attributes']['SeatsCount']
-        # if seats_count == 0:    # incorrect seats value
-        #    continue
-        if min_seats_count < seats_count:
-            continue
-        if min_seats_count > seats_count:
-            min_seats_count = seats_count
-            smallest_bars = []
-        smallest_bars.append(bar)
-    return smallest_bars
+    seats_count = lambda x: x['properties']['Attributes']['SeatsCount']
+    return min(data['features'], key=seats_count)
 
 
 def get_closest_bar(data, longitude, latitude):
-    min_distance = float(sys.maxint)
-    closest_bar = {}
-    for bar in data['features']:
+
+    def get_distance(bar):
         longitude_delta = bar['geometry']['coordinates'][0] - longitude
         latitude_delta = bar['geometry']['coordinates'][1] - latitude
-        distance = math.hypot(longitude_delta, latitude_delta)
-        if min_distance > distance:
-            min_distance = distance
-            closest_bar = bar
-    return closest_bar
+        return math.hypot(longitude_delta, latitude_delta)
+
+    return min(data['features'], key=get_distance)
 
 
 def get_bar_name(data):
@@ -58,13 +34,9 @@ def get_bar_name(data):
 if __name__ == '__main__':
     moscow_bars = load_data('bars.json')
 
-    print('Biggest bars:')
-    for bar in get_biggest_bar(moscow_bars):
-        print(get_bar_name(bar))
+    print('Biggest bar: ' + get_bar_name(get_biggest_bar(moscow_bars)))
 
-    print('Smallest bars:')
-    for bar in get_smallest_bar(moscow_bars):
-        print(get_bar_name(bar))
+    print('Smallest bar: ' + get_bar_name(get_smallest_bar(moscow_bars)))
 
     user_longitude = input('Input your longitude: ')
     user_latitude = input('Input your latitude: ')
