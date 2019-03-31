61b3da98b8cfa10490a2b4d0b303f48c85a05dc8
cr00z <netrebinr@gmail.com>
2019-03-31 21:44:33 +0700

    make seventh attempt

--- bars.py
+++ bars.py
@@ -1,11 +1,12 @@
 import json
 import math
-import io
+import argparse
+import functools
 
 
 def load_data(filepath):
     try:
-        with io.open(filepath, encoding='utf-8') as json_file_object:
+        with open(filepath, encoding='utf-8') as json_file_object:
             return json.load(json_file_object)
     except (FileNotFoundError, json.decoder.JSONDecodeError):
         return None
@@ -27,28 +28,36 @@ def get_smallest_bar(bars_list):
     return min(bars_list, key=get_bar_seats_count)
 
 
-def get_distance(bar):
+def get_distance(latitude, longitude, bar):
     bar_coord = bar['geometry']['coordinates']
-    longitude_delta = bar_coord[0] - user_longitude
-    latitude_delta = bar_coord[1] - user_latitude
+    longitude_delta = bar_coord[0] - longitude
+    latitude_delta = bar_coord[1] - latitude
     return math.hypot(longitude_delta, latitude_delta)
 
 
-def get_closest_bar(bars_list):
-    return min(bars_list, key=get_distance)
+def get_closest_bar(bars_list, latitude, longitude):
+    calc_distance = functools.partial(get_distance, latitude, longitude)
+    return min(bars_list, key=calc_distance)
 
 
 if __name__ == '__main__':
-    moscow_bars = load_data('bars.json')['features']
+    parser = argparse.ArgumentParser(description='Simple Moscow bars search')
+    parser.add_argument('json_filename', metavar='json_filename', type=str,
+                        help='JSON database filename')
+    args = parser.parse_args()
+
+    moscow_bars = load_data(args.json_filename)
     if moscow_bars is None:
         exit('Input file not found or not a JSON')
+    moscow_bars = moscow_bars['features']
+
     print('Biggest bar: {}'.format(get_bar_name(get_biggest_bar(moscow_bars))))
     print('Smallest bar: {}'.format(get_bar_name(get_smallest_bar(moscow_bars))))
     try:
-        # SIC!!! in Russia we use latitude first
+        # NB! in Russia we use latitude first
         user_latitude = float(input('Input your latitude: '))
         user_longitude = float(input('Input your longitude: '))
     except ValueError:
         exit('Coordinates must be digital')
-    closest_bar = get_closest_bar(moscow_bars)
+    closest_bar = get_closest_bar(moscow_bars, user_latitude, user_longitude)
     print('Closest bar: {}'.format(get_bar_name(closest_bar)))
